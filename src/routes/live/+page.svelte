83090ee147a36ed8d088066b600e937b59f7e14f
Supa <36031171+0Supa@users.noreply.github.com>
2024-12-23 23:15:56 +0200

    impr(directory): adjust thumbnail interval

--- src/routes/live/+page.svelte
+++ src/routes/live/+page.svelte
@@ -41,7 +41,7 @@
                 <a href="https://www.twitch.tv/{stream.login}" target="_blank">
                     <Card.Root class="text-left p-1 hover:scale-105 bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-900 dark:hover:bg-neutral-800 transition">
                         <div class="max-w-full">
-                            <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_{stream.login}-600x338.jpg?t={Date.now()}" loading="lazy" alt="Thumbnail" class="rounded-sm aspect-video w-full _thumbnail" />
+                            <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_{stream.login}-600x338.jpg?t={~~(Date.now() / 1000 / 120)}" loading="lazy" alt="Thumbnail" class="rounded-sm aspect-video w-full _thumbnail" />
                             <div class="flex h-12 items-center mt-0 mx-1">
                                 <img src={stream.avatar} loading="lazy" alt="Avatar" class="mr-1 rounded-full size-10 text-center" />
 
