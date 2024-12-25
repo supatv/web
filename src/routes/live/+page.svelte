57f0cd1cc462585b9dc09c386c7a7ccea70c9999
Supa <36031171+0Supa@users.noreply.github.com>
2024-12-25 20:16:46 +0200

    feat(live): uptime

--- src/routes/live/+page.svelte
+++ src/routes/live/+page.svelte
@@ -8,6 +8,20 @@
 
     import { onDestroy, onMount } from "svelte";
 
+    const formatUptime = (s: string) => {
+        let string = "";
+        const date = Date.parse(s);
+        const t = Date.now() - date;
+
+        const seconds = Math.floor(t / 1000);
+        const hours = Math.floor(seconds / (60 * 60));
+        const minutes = Math.floor(seconds / 60 - hours * 60);
+
+        if (hours) string += `${hours}h`;
+        if (minutes) string += `${minutes}m`;
+        return string;
+    };
+
     let fetchInterval: number | null = null;
 
     let streams: { uid: string; login: string; name: string; started: string; viewers: number; game: string; title: string; avatar: string; type: string }[] | null = null;
@@ -43,7 +57,11 @@
                 <a href="https://www.twitch.tv/{stream.login}" target="_blank">
                     <Card.Root class="text-left p-1 hover:scale-105 bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-900 dark:hover:bg-neutral-800 transition">
                         <div class="max-w-full">
-                            <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_{stream.login}-600x338.jpg?t={~~(Date.now() / 1000 / 120)}" loading="lazy" alt="Thumbnail" class="rounded-sm aspect-video w-full _thumbnail" />
+                            <div class="relative rounded-sm overflow-hidden">
+                                <span class="tabular-nums text-xs absolute right-0 bg-black/50 text-white px-0.5 rounded-bl-sm">{formatUptime(stream.started)}</span>
+                                <img src="https://static-cdn.jtvnw.net/previews-ttv/live_user_{stream.login}-600x338.jpg?t={~~(Date.now() / 1000 / 120)}" loading="lazy" alt="Thumbnail" class="aspect-video w-full _thumbnail" />
+                            </div>
+
                             <div class="flex h-12 items-center mt-0 mx-1">
                                 <img src={stream.avatar} loading="lazy" alt="Avatar" class="mr-1 rounded-full size-10 text-center" />
 
@@ -52,7 +70,7 @@
                                         <p class="font-semibold gap-0.5 flex items-center" title={stream.name}>
                                             {stream.name}
                                             {#if stream.type == "partner"}
-                                                <IconCheck class="inline h-4 w-4 text-purple-500 dark:text-purple-300" />
+                                                <IconCheck class="inline size-4 text-purple-500 dark:text-purple-300" />
                                             {/if}
                                         </p>
                                         <p class="text-purple-500 dark:text-purple-300 capitalize text-sm">{stream.type}</p>
@@ -60,7 +78,7 @@
 
                                     <div class="text-right overflow-hidden">
                                         <div class="flex justify-end items-center text-red-500 dark:text-red-300 font-semibold">
-                                            <IconUser class="h-5 w-5" />
+                                            <IconUser class="size-5" />
                                             <span>{stream.viewers.toLocaleString()}</span>
                                         </div>
                                         <p class="text-sm opacity-80 whitespace-nowrap overflow-hidden text-ellipsis" title={stream.game}>{stream.game}</p>
