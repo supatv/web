49d7b17b2d22c3a36be925291ce1cd6c18f00ead
Supa <36031171+0Supa@users.noreply.github.com>
2025-04-02 00:32:45 +0300

    tweak(image): add loading style onMount

--- src/lib/components/image.svelte
+++ src/lib/components/image.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     let props = $props();
 
     let ref: HTMLImageElement;
+
     const onComplete = (event: Event) => {
         ref.classList.remove("animate-pulse");
         if (event.type !== "error") ref.classList.remove("bg-muted");
     };
+
+    onMount(() => {
+        if (!ref.complete) ref.classList.add("bg-muted", "animate-pulse");
+    });
 </script>
 
-<img bind:this={ref} onload={onComplete} onerror={onComplete} {...props} class="text-center bg-muted animate-pulse {props?.class}" />
+<img bind:this={ref} onload={onComplete} onerror={onComplete} {...props} class="text-center {props?.class}" />
