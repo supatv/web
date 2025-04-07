7518a35d58d4855776858c36f3bf427edd97f6eb
Supa <36031171+0Supa@users.noreply.github.com>
2025-04-07 10:33:44 +0300

    fix(vods): ensure lowercase channel

--- src/routes/vods/[channel]/+page.svelte
+++ src/routes/vods/[channel]/+page.svelte
@@ -15,8 +15,6 @@
 	import relativeTime from "dayjs/plugin/relativeTime";
 	import duration from "dayjs/plugin/duration";
 
-	page.params.channel = page.params.channel.toLowerCase();
-
 	const title = getContext<TitleContext>("title");
 
 	dayjs.extend(relativeTime);
--- src/routes/vods/[channel]/+page.ts
+++ src/routes/vods/[channel]/+page.ts
@@ -3,6 +3,8 @@ import type { PageLoad } from "./$types";
 import type { User } from "$lib/twitch/streams";
 
 export const load: PageLoad = async ({ params }) => {
+	params.channel = params.channel.toLowerCase();
+
 	const res = await fetch(`https://api-tv.supa.sh/user?login=${encodeURIComponent(params.channel)}`);
 	if (!res.ok) {
 		throw error(res.status, res.status === 404 ? "User not found" : `Failed to load user: ${res.statusText}`);
