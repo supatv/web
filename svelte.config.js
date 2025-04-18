a132ba2c2cf16506ad449051c17c8bfec5e42d31
Supa <36031171+0Supa@users.noreply.github.com>
2025-04-18 22:21:01 +0300

    chore(svelte): use static adapter for dev

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import cloudflareAdapter from "@sveltejs/adapter-cloudflare";
+import staticAdapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +7,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: process.env.NODE_ENV === "production" ? cloudflareAdapter() : staticAdapter(),
 		prerender: {
 			origin: "https://tv.supa.sh",
 		},
