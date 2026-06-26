548ffe85a49c966b1a65b8381bc15ba218104177
Supa <supa@juliaelena.ro>
2026-06-26 10:17:33 +0300

    chore(player): segment prefetch hack for kick

--- src/lib/components/live/stream-player.svelte
+++ src/lib/components/live/stream-player.svelte
@@ -29,7 +29,7 @@
 			callbacks.onSuccess = (response, stats, context, networkDetails) => {
 				if (context.type === "level") {
 					if (response.data.includes("#EXT-X-MAP")) hls.config.progressive = false;
-					else response.data = response.data.replace(/#EXT-X-TWITCH-PREFETCH:(.+)/g, "#EXTINF:2.0,\n$1");
+					else response.data = response.data.replace(/#EXT-X-(?:TWITCH-)?PREFETCH:(.+)/g, "#EXTINF:2.0,\n$1");
 				} else if (context.type === "manifest") {
 					manifestMap.set(channelName, response.data);
 				}
